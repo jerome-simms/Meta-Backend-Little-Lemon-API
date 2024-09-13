8a74fc1b3eb199c5746a8f223f383826b5d63941
Jerome Simms <jerome.tech57@gmail.com>
2024-09-13 18:33:27 +0100

    Add basic throttling to the API

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -145,6 +145,15 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ],
 
+    'DEFUALT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/hour',
+        'user': '100/hour'
+    },
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
